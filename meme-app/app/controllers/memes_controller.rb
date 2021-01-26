636c8996fb1897e51c918961635ffde3e1380c95
brian4548 <brianr4548@gmail.com>
2021-01-26 15:55:30 -0600

    create meme

--- meme-app/app/controllers/memes_controller.rb
+++ meme-app/app/controllers/memes_controller.rb
@@ -1,7 +1,24 @@
 class MemesController < ApplicationController
 
-   def index 
-      render json: Meme.all
+   def index  
+      render json: Meme.all.to_json({
+         include:{
+            comments:{
+               execpt: [:updated_at]
+            }
+         },
+         except: [:created_at, :updated_at]
+      })
+   end
+
+   def create
+      meme = Meme.create(meme_params)
+      render json: meme
+   end
+
+   def meme_params
+      params.require(:meme).permit(:title, :description, :img_url)
+
    end
 
 
